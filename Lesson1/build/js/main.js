f4c069610a71431b0de48b5195d985d177e3ba2b
Karthik Veerla <karthikveerla28@gmail.com>
2026-04-09 23:51:50 -0400

    .

--- Lesson1/build/js/main.js
+++ Lesson1/build/js/main.js
@@ -3,3 +3,8 @@ let username = 'karthik';
 console.log(username);
 let param = 'karthik';
 param = false;
+let stringarr = ['karthik', 'sai', 'harish'];
+let numarr = [1, 2, 3, 4, 5];
+let boolarr = [true, false, true];
+let strnumarr = ['karthik', 1, 'sai', 2, 'harish', 3];
+let strnumboolarr = ['karthik', 1, true, 'sai', 2, false, 'harish', 3, true];
