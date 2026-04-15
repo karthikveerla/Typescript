ebad1f28c6403c1c3fa892f7f9ebfded91ec28ca
Karthik Veerla <karthikveerla28@gmail.com>
2026-04-14 22:52:23 -0400

    functions

--- Lesson1/build/js/main.js
+++ Lesson1/build/js/main.js
@@ -1,22 +1,51 @@
 "use strict";
-let username = 'karthik';
-console.log(username);
-let myobj;
-myobj = {
-    name: 'karthik',
-    age: 25,
-    email: 'karthik@example.com',
-    phone: '123-456-7890',
-    address: '123 Main St, Anytown, USA'
+// let username = 'karthik';
+// console.log(username);
+// let myobj: object;
+// myobj ={
+//     name: 'karthik',
+//     age: 25,
+//     email: 'karthik@example.com',   
+//     phone: '123-456-7890',
+//     address: '123 Main St, Anytown, USA'
+// }
+// type employee ={
+//     name: string;
+//     id: number;
+//     department: string;
+//     salary: number;
+//     skills: string[];
+// }
+// let emp1: employee = {
+//     name: 'karthik',
+//     id: 101,
+//     department: 'IT',
+//     salary: 50000,
+//     skills:['java','python','springboot']
+// }
+// const greetemp = (emp:employee) =>{
+//     return `Hello ${emp.name}`;
+// }
+// console.log(greetemp(emp1));
+//enums
+var color;
+(function (color) {
+    color[color["red"] = 1] = "red";
+    color[color["green"] = 2] = "green";
+    color[color["blue"] = 3] = "blue";
+})(color || (color = {}));
+console.log(color.red);
+let dir = 'up';
+function move(direction) {
+    console.log(`Moving ${direction}`);
+}
+move('up');
+move('left');
+const add = (a, b) => {
+    return a + b;
 };
-let emp1 = {
-    name: 'karthik',
-    id: 101,
-    department: 'IT',
-    salary: 50000,
-    skills: ['java', 'python', 'springboot']
+const printmsg = (msg) => {
+    console.log(msg);
 };
-const greetemp = (emp) => {
-    return `Hello ${emp.name}`;
-};
-console.log(greetemp(emp1));
+printmsg('arrow functions!');
+printmsg(add(3, 6));
--- Lesson1/src/main.ts
+++ Lesson1/src/main.ts
@@ -1,32 +1,62 @@
-let username = 'karthik';
-console.log(username);
+// let username = 'karthik';
+// console.log(username);
 
-let myobj: object;
-myobj ={
-    name: 'karthik',
-    age: 25,
-    email: 'karthik@example.com',   
-    phone: '123-456-7890',
-    address: '123 Main St, Anytown, USA'
+// let myobj: object;
+// myobj ={
+//     name: 'karthik',
+//     age: 25,
+//     email: 'karthik@example.com',   
+//     phone: '123-456-7890',
+//     address: '123 Main St, Anytown, USA'
+// }
+
+// type employee ={
+//     name: string;
+//     id: number;
+//     department: string;
+//     salary: number;
+//     skills: string[];
+// }
+
+// let emp1: employee = {
+//     name: 'karthik',
+//     id: 101,
+//     department: 'IT',
+//     salary: 50000,
+//     skills:['java','python','springboot']
+// }
+
+// const greetemp = (emp:employee) =>{
+//     return `Hello ${emp.name}`;
+// }
+// console.log(greetemp(emp1));
+
+
+//enums
+enum color{
+    red = 1,
+    green,
+    blue
+}
+console.log(color.red);  
+
+//literal types
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+let dir: Direction = 'up';
+function move(direction: Direction) {
+    console.log(`Moving ${direction}`);
+}   
+move('up');
+move('left');
+
+const add = (a:number, b:number): number=>{
+    return a+b;
 }
 
-type employee ={
-    name: string;
-    id: number;
-    department: string;
-    salary: number;
-    skills: string[];
+const printmsg = (msg:any):void =>{
+    console.log(msg);
 }
 
-let emp1: employee = {
-    name: 'karthik',
-    id: 101,
-    department: 'IT',
-    salary: 50000,
-    skills:['java','python','springboot']
-}
-
-const greetemp = (emp:employee) =>{
-    return `Hello ${emp.name}`;
-}
-console.log(greetemp(emp1));
+printmsg('arrow functions!');
+printmsg(add(3,6));
