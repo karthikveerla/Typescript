22ef60dfaced511fdbb7c8332cef4b95432aeba1
Karthik Veerla <karthikveerla28@gmail.com>
2026-04-16 23:50:42 -0400

    assertions

--- Lesson1/build/js/main.js
+++ Lesson1/build/js/main.js
@@ -1,69 +1,86 @@
 "use strict";
-// let username = 'karthik';
-// console.log(username);
-// let myobj: object;
-// myobj ={
-//     name: 'karthik',
-//     age: 25,
-//     email: 'karthik@example.com',   
-//     phone: '123-456-7890',
-//     address: '123 Main St, Anytown, USA'
-// }
-// type employee ={
-//     name: string;
-//     id: number;
-//     department: string;
-//     salary: number;
-//     skills: string[];
-// }
-// let emp1: employee = {
-//     name: 'karthik',
-//     id: 101,
-//     department: 'IT',
-//     salary: 50000,
-//     skills:['java','python','springboot']
-// }
-// const greetemp = (emp:employee) =>{
-//     return `Hello ${emp.name}`;
-// }
-// console.log(greetemp(emp1));
-//enums
-var color;
-(function (color) {
-    color[color["red"] = 1] = "red";
-    color[color["green"] = 2] = "green";
-    color[color["blue"] = 3] = "blue";
-})(color || (color = {}));
-console.log(color.red);
-let dir = 'up';
-function move(direction) {
-    console.log(`Moving ${direction}`);
-}
-move('up');
-move('left');
-const add = (a, b) => {
-    return a + b;
-};
-const printmsg = (msg) => {
-    console.log(msg);
-};
-printmsg('arrow functions!');
-printmsg(add(3, 6));
-//optionalparamenter
-const addAll = (a, b, c) => {
-    if (c != undefined) {
-        return a + b + c;
+let a = 'Sanuel';
+let b = a;
+let c = a;
+let d = 'Samuel';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add') {
+        return a + b;
     }
-    return a + b;
+    return '' + a + b;
 };
-//default parameter
-const addWithDefault = (a, b, c = 10) => {
-    return a + b + c;
-};
-const addwithfirstdefault = (a = 10, b) => {
-    return a + b;
-};
-console.log(addAll(3, 4));
-console.log(addAll(3, 4, 5));
-console.log(addWithDefault(3, 4));
-console.log(addwithfirstdefault(undefined, 5));
+let myval = addOrConcat(2, 3, 'concat');
+console.log(myval);
+let myval2 = addOrConcat(2, 3, 'add');
+console.log(myval2);
+// // let username = 'karthik';
+// // console.log(username);
+// // let myobj: object;
+// // myobj ={
+// //     name: 'karthik',
+// //     age: 25,
+// //     email: 'karthik@example.com',   
+// //     phone: '123-456-7890',
+// //     address: '123 Main St, Anytown, USA'
+// // }
+// // type employee ={
+// //     name: string;
+// //     id: number;
+// //     department: string;
+// //     salary: number;
+// //     skills: string[];
+// // }
+// // let emp1: employee = {
+// //     name: 'karthik',
+// //     id: 101,
+// //     department: 'IT',
+// //     salary: 50000,
+// //     skills:['java','python','springboot']
+// // }
+// // const greetemp = (emp:employee) =>{
+// //     return `Hello ${emp.name}`;
+// // }
+// // console.log(greetemp(emp1));
+// //enums
+// enum color{
+//     red = 1,
+//     green,
+//     blue
+// }
+// console.log(color.red);  
+// //literal types
+// type Direction = 'up' | 'down' | 'left' | 'right';
+// let dir: Direction = 'up';
+// function move(direction: Direction) {
+//     console.log(`Moving ${direction}`);
+// }   
+// move('up');
+// move('left');
+// const add = (a:number, b:number): number=>{
+//     return a+b;
+// }
+// const printmsg = (msg:any):void =>{
+//     console.log(msg);
+// }
+// printmsg('arrow functions!');
+// printmsg(add(3,6));
+// //optionalparamenter
+// const addAll = (a:number,b:number,c?:number) : number =>{
+//     if(c != undefined){
+//         return a+b+c;
+//     }
+//     return a+b;
+// }
+// //default parameter
+// const addWithDefault = (a:number,b:number,c:number=10): number =>{
+//     return a+b+c;
+// }
+// const addwithfirstdefault = (a:number =10,b:number):number=>{
+//     return a+b;
+// }
+// console.log(addAll(3,4));
+// console.log(addAll(3,4,5));
+// console.log(addWithDefault(3,4));
+// console.log(addwithfirstdefault(undefined,5));
+// type numberOrString = number | string;
+// let value: numberOrString;
--- Lesson1/src/main.ts
+++ Lesson1/src/main.ts
@@ -1,84 +1,114 @@
-// let username = 'karthik';
-// console.log(username);
+type one = string
+type two = string
+type three = number | string
 
-// let myobj: object;
-// myobj ={
-//     name: 'karthik',
-//     age: 25,
-//     email: 'karthik@example.com',   
-//     phone: '123-456-7890',
-//     address: '123 Main St, Anytown, USA'
-// }
+let a: one = 'Sanuel'
+let b = a as two
+let c = a as three
 
-// type employee ={
-//     name: string;
-//     id: number;
-//     department: string;
-//     salary: number;
-//     skills: string[];
-// }
-
-// let emp1: employee = {
-//     name: 'karthik',
-//     id: 101,
-//     department: 'IT',
-//     salary: 50000,
-//     skills:['java','python','springboot']
-// }
-
-// const greetemp = (emp:employee) =>{
-//     return `Hello ${emp.name}`;
-// }
-// console.log(greetemp(emp1));
+let d = <one>'Samuel'
 
 
-//enums
-enum color{
-    red = 1,
-    green,
-    blue
-}
-console.log(color.red);  
-
-//literal types
-type Direction = 'up' | 'down' | 'left' | 'right';
-
-let dir: Direction = 'up';
-function move(direction: Direction) {
-    console.log(`Moving ${direction}`);
-}   
-move('up');
-move('left');
-
-const add = (a:number, b:number): number=>{
-    return a+b;
-}
-
-const printmsg = (msg:any):void =>{
-    console.log(msg);
-}
-
-printmsg('arrow functions!');
-printmsg(add(3,6));
-
-//optionalparamenter
-const addAll = (a:number,b:number,c?:number) : number =>{
-    if(c != undefined){
-        return a+b+c;
+const addOrConcat = (a:number,b:number, c: 'add' | 'concat') : number | string =>{
+    if(c === 'add'){
+        return a+b;
     }
-    return a+b;
+    return ''+a+b;
 }
 
-//default parameter
-const addWithDefault = (a:number,b:number,c:number=10): number =>{
-    return a+b+c;
-}
+let myval: string = addOrConcat(2,3,'concat') as string;
+console.log(myval);
 
-const addwithfirstdefault = (a:number =10,b:number):number=>{
-    return a+b;
-}
+let myval2: number = addOrConcat(2,3,'add') as number;
+console.log(myval2);
 
-console.log(addAll(3,4));
-console.log(addAll(3,4,5));
-console.log(addWithDefault(3,4));
-console.log(addwithfirstdefault(undefined,5));
+
+
+// // let username = 'karthik';
+// // console.log(username);
+
+// // let myobj: object;
+// // myobj ={
+// //     name: 'karthik',
+// //     age: 25,
+// //     email: 'karthik@example.com',   
+// //     phone: '123-456-7890',
+// //     address: '123 Main St, Anytown, USA'
+// // }
+
+// // type employee ={
+// //     name: string;
+// //     id: number;
+// //     department: string;
+// //     salary: number;
+// //     skills: string[];
+// // }
+
+// // let emp1: employee = {
+// //     name: 'karthik',
+// //     id: 101,
+// //     department: 'IT',
+// //     salary: 50000,
+// //     skills:['java','python','springboot']
+// // }
+
+// // const greetemp = (emp:employee) =>{
+// //     return `Hello ${emp.name}`;
+// // }
+// // console.log(greetemp(emp1));
+
+
+// //enums
+// enum color{
+//     red = 1,
+//     green,
+//     blue
+// }
+// console.log(color.red);  
+
+// //literal types
+// type Direction = 'up' | 'down' | 'left' | 'right';
+
+// let dir: Direction = 'up';
+// function move(direction: Direction) {
+//     console.log(`Moving ${direction}`);
+// }   
+// move('up');
+// move('left');
+
+// const add = (a:number, b:number): number=>{
+//     return a+b;
+// }
+
+// const printmsg = (msg:any):void =>{
+//     console.log(msg);
+// }
+
+// printmsg('arrow functions!');
+// printmsg(add(3,6));
+
+// //optionalparamenter
+// const addAll = (a:number,b:number,c?:number) : number =>{
+//     if(c != undefined){
+//         return a+b+c;
+//     }
+//     return a+b;
+// }
+
+// //default parameter
+// const addWithDefault = (a:number,b:number,c:number=10): number =>{
+//     return a+b+c;
+// }
+
+// const addwithfirstdefault = (a:number =10,b:number):number=>{
+//     return a+b;
+// }
+
+// console.log(addAll(3,4));
+// console.log(addAll(3,4,5));
+// console.log(addWithDefault(3,4));
+// console.log(addwithfirstdefault(undefined,5));
+
+// type numberOrString = number | string;
+
+// let value: numberOrString;
